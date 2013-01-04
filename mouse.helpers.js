bd52ecda756944ada0ebf91b8a67efc84fb300ea
Samuel Charron <samuel.charron@gmail.com>
2013-01-04 15:19:47 +0100

    Add units and change separators. Thanks glebourg.

--- mouse.helpers.js
+++ mouse.helpers.js
@@ -1,5 +1,5 @@
 function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 }
 
 var select_station = function(station) {
